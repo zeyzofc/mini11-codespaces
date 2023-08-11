e21c8acd74210c18438b5a0e0b8ec3154b538ce8
vichingo455-temp <142008395+vichingo455-temp@users.noreply.github.com>
2023-08-11 18:46:10 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt install qemu qemu-system-x86 aria2 -y
+aria2c -x16 -s16 -oDVD.iso https://archive.org/download/mini11-22h2/Mini11%20AIO%2022H2%20Beta%202%20Longhorn.iso
+qemu-img create -f qcow2 disk.img 25G
+qemu-system-x86_64 -m 10240 -hda disk.img -vnc 127.0.0.1:1 -boot c -usbdevice tablet -cdrom DVD.iso -netdev user,id=n1 -device e1000,netdev=n1 & ./noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:80
